a26893bfdddd2fe0cfde8334885b5ad68f68643e
root <root@pve>
2026-04-18 19:40:17 +0200

    loadmodules: write log output to /dev/console for serial visibility

--- cmd/loadmodules/main.go
+++ cmd/loadmodules/main.go
@@ -14,17 +14,25 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
 	"golang.org/x/sys/unix"
 )
 
+func openConsole() *os.File {
+	f, err := os.OpenFile("/dev/console", os.O_WRONLY, 0)
+	if err != nil {
+		return os.Stderr
+	}
+	return f
+}
+
 func release() string {
 	var uts unix.Utsname
 	if err := unix.Uname(&uts); err != nil {
-		fmt.Fprintf(os.Stderr, "loadmodules: uname: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("uname: %v", err)
 	}
 	return string(uts.Release[:bytes.IndexByte(uts.Release[:], 0)])
 }
@@ -38,19 +46,23 @@ func loadModule(rel, mod string) error {
 	defer f.Close()
 	if err := unix.FinitModule(int(f.Fd()), "", 0); err != nil {
 		if err == unix.EEXIST || err == unix.EBUSY {
-			fmt.Fprintf(os.Stderr, "loadmodules: %s: already loaded\n", mod)
+			log.Printf("%s: already loaded", mod)
 			return nil
 		}
-		return fmt.Errorf("FinitModule(%s): %v", mod, err)
+		return fmt.Errorf("finit_module(%s): %v", mod, err)
 	}
-	fmt.Fprintf(os.Stderr, "loadmodules: %s: loaded\n", mod)
+	log.Printf("%s: loaded", mod)
 	return nil
 }
 
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix("loadmodules: ")
+	log.SetOutput(openConsole())
+
 	modules := os.Args[1:]
 	if len(modules) == 0 {
-		fmt.Fprintf(os.Stderr, "loadmodules: no modules specified, nothing to do\n")
+		log.Print("no modules specified, nothing to do")
 		os.Exit(125)
 	}
 
@@ -58,7 +70,7 @@ func main() {
 	var failed bool
 	for _, mod := range modules {
 		if err := loadModule(rel, mod); err != nil {
-			fmt.Fprintf(os.Stderr, "loadmodules: %v\n", err)
+			log.Print(err)
 			failed = true
 		}
 	}
